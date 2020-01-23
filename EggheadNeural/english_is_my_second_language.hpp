82b88ff1e80288001c3599dea3fb5cfb9b24807e
Krobix <45689440+Krobix@users.noreply.github.com>
2020-01-23 11:14:02 -0500

    Add files via upload

---
+++ EggheadNeural/english_is_my_second_language.hpp
@@ -0,0 +1,30 @@
+﻿//normal --> nomral
+
+#ifndef ENGLISH
+#define int itn
+#define main mian
+#define using usign
+#define namespace naemspace
+#define std sdt
+#define std::cout std::cuot
+#define std::string std::srting
+#define true treu
+#define false fasle
+#define class clazs
+#define bool boul
+#endif
+//nomral --> normal
+
+#ifdef ENGLISH
+#define itn int
+#define mian main
+#define usign using
+#define naemspace namespace
+#define sdt std
+#define std::cuot std::cout
+#define std::srting std::string
+#define treu true
+#define fasle false
+#define clazs class
+#define boul bool
+#endif
---
+++ EggheadNeural/hello_im_an_atheist.hpp
@@ -0,0 +1,7 @@
+﻿#define Hello int
+#define Im main
+#define an (
+#define atheist )
+#define There_is_no return
+#define God 0
+#define and_thats_a_fact ;
